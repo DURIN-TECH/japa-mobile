b8e6f9102aaa42b2367717e201f2bf6b85d485c4
wrightkhlebisol <calebogundiya@gmail.com>
2025-01-17 13:08:45 +0000

    temporary disable onboarding

--- (onboard)/_layout.tsx
+++ (onboard)/_layout.tsx
@@ -8,7 +8,7 @@ export default function OnboardingLayout() {
 
   return (
     <Stack>
-      {/* <Stack.Screen name="index" options={{ headerShown: false }} /> */}
+      <Stack.Screen name="index" options={{ headerShown: false }} />
     </Stack>
   );
 }
